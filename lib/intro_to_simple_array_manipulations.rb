83294a2644d25b98923adb3db853287b14dfd5cd
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-21 01:40:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+   array = ('neal', 'noah', 'nolan')
+   array.pop
+
+
+
+end
+
+def pop_with_args(array)
+   pop_with_args.pop(2)
+end
